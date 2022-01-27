b53807d1141674859ae855cee451f546f1ceb2cd
RobMiller329 <robert.miller.329@gmail.com>
2022-01-26 23:07:32 -0500

    overhauling apis

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const cors = require("cors");
+const bodyParser = require("body-parser")
 const cookbookDB = require('./queries');
 
 router.use(cors());
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded( { extended: false } ));
 
-//api call for viewing user's recipes
-router.get('/:id', async (req, res, next) =>
+/*
+    10) update ingredient data              /ingredientsdata/update/
+    11) update instruction data             /instructionsdata/update/    
+    13) update user data                    /userdata/update/
+
+    14) delete recipe details               /recipedata/delete/
+    15) delete recipe ingredients           /ingredientsdata/delete/
+    16) delete recipe instructions          /instructionsdata/delete/
+
+    06) insert recipe data                  /recipedata/insert/
+    07) insert ingredient data              /ingredientsdata/insert/
+    08) insert instructions data            /instructionsdata/insert/
+    12) insert user data                    /userdata/insert/
+    
+*/
+
+//api call for fetching all recipes
+router.get('/browse/', async (req, res) =>
 {
     try
     {
-        let results = await cookbookDB.viewRecipes(req.params.id);
+        let results = await cookbookDB.browseRecipes();
         res.json(results);
     }catch(err)
     {
@@ -19,22 +38,8 @@ router.get('/:id', async (req, res, next) =>
     }
 });
 
-//api call for viewing all recipes
-router.get('/', async (req, res, next) =>
-{
-    try
-    {
-        let results = await cookbookDB.viewRecipes();
-        res.json(results);
-    }catch(err)
-    {
-        console.log(err);
-        res.sendStatus(500);
-    }
-});
-
-//api call for viewing list of user's recipes
-router.get('/recipeList/:id', async (req, res, next) =>
+//api call for fetching list of user's recipes
+router.get('/recipeList/:id', async (req, res) =>
 {
     try
     {
@@ -47,12 +52,12 @@ router.get('/recipeList/:id', async (req, res, next) =>
     }
 });
 
-//api call for viewing user's selected recipe details
-router.get('/recipeIngredients/:id', async (req, res, next) =>
+//api call for fetching user's selected recipe ingredients in the edit component
+router.get('/recipeIngredients/select/:id', async (req, res) =>
 {
     try
     {
-        let results = await cookbookDB.detailIngredientsRecipes(req.params.id);
+        let results = await cookbookDB.fetchRecipeIngredients(req.params.id);
         res.json(results);
     }catch(err)
     {
@@ -61,12 +66,12 @@ router.get('/recipeIngredients/:id', async (req, res, next) =>
     }
 });
 
-//api call for viewing user's selected recipe details
-router.get('/recipeInstructions/:id', async (req, res, next) =>
+//api call for fetching user's selected recipe instructions in the edit component
+router.get('/recipeInstructions/select/:id', async (req, res, next) =>
 {
     try
     {
-        let results = await cookbookDB.detailInstructionsRecipes(req.params.id);
+        let results = await cookbookDB.fetchRecipeInstructions(req.params.id);
         res.json(results);
     }catch(err)
     {
@@ -75,18 +80,76 @@ router.get('/recipeInstructions/:id', async (req, res, next) =>
     }
 });
 
-//api call for creating transaction record of user's new recipe
-router.get('/recipeInstructions/:transactionID, transactionTime, userDataID, recipeDataID', async (req, res, next) =>
+//api call for updating selected recipe data
+router.get('/recipedata/update/', async (req, res, next) =>
 {
     try
     {
-        let results = await cookbookDB.detailInstructionsRecipes(req.params.id);
+        let results = await cookbookDB.updateRecipeData(req.body.source, req.body.userID, req.body.rName, req.body.rCuisine, req.body.rProtein, req.body.recID);
         res.json(results);
     }catch(err)
     {
         console.log(err);
         res.sendStatus(500);
     }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+router.post('/username/', async (req, res) =>
+{
+    try
+    {
+        let apiCall = await cookbookDB.createUsername(req.body.email, req.body.username);
+        console.log(req.body.email, req.body.username);
+    }catch(err)
+    {
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+
+//
+router.post('/createRecipe/transactions/', async (req, res, next) =>
+{
+    console.log("req:"+req.body);
+    console.log("res"+res);
+    /* try
+    {
+        let results = await cookbookDB.createNewRecipeTransaction(req.params.tranString);
+        res.json(results);
+    }catch(err)
+    {
+        console.log(err);
+        res.sendStatus(500);
+    } */
+});
+
+//
+router.post('/createRecipe/recipedata/', async (req, res, next) =>
+{
+    console.log("req:"+req.body);
+    console.log("res"+res);
+    /* try
+    {
+        let results = await cookbookDB.createNewRecipeTransaction(req.params.tranString);
+        res.json(results);
+    }catch(err)
+    {
+        console.log(err);
+        res.sendStatus(500);
+    } */
 });
 
 module.exports = router;
