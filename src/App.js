0d3b94e406cf681be9d9e478bf55faa8d4ac15b7
RobMiller329 <robert.miller.329@gmail.com>
2022-03-11 14:31:44 -0500

    small update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import React from "react";
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageHeader from "./PageHeader";
 import PageFooter from "./PageFooter";
 import PageContent from "./PageContent";
 import Navigation from "./contentComponents/ContentNavigation";
+
+import NewRecipeForm from "./formComponents/FormNewRecipe";
+import EditRecipesForm from "./formComponents/FormEditRecipes";
+import Settings from "./contentComponents/Settings";
+import BrowseTableBuild from "./contentComponents/BrowseTableBuild";
+import { Account } from "./loginComponents/Account";
 import './App.css';
+
+function BrowsingRecipesTable(props)
+{
+    return(
+        <Account>
+            <BrowseTableBuild />
+        </Account>
+    );
+}
+
+function NewRecipeFormPage(props)
+{
+    return(
+        <Account>
+            <NewRecipeForm />
+        </Account>
+    );
+}
+
+function EditRecipesFormPage(props)
+{
+    return(
+        <Account>
+            <EditRecipesForm />
+        </Account>
+    );
+}
+
+function SettingsPage(props)
+{
+    return(
+        <Account>
+            <Settings />
+        </Account>
+    );
+}
 
 function App()
 {
@@ -17,20 +60,28 @@ function App()
   }
 
   return(
-    <div className="App">
-      <div className="appHeader">
-        <PageHeader onPageChange={handlePageChange} />
+    <BrowserRouter>
+      <div className="App">
+        <div className="appHeader">
+          <PageHeader onPageChange={handlePageChange} />
+        </div>
+        <div className="appNavigation">
+          <Navigation page={page} onPageChange={handlePageChange} />
+        </div>
+        <div className="appBody">
+          <PageContent page={page} />
+        </div>
+        <div className="appFooter">
+          <PageFooter />
+        </div>
       </div>
-      <div className="appNavigation">
-        <Navigation page={page} onPageChange={handlePageChange} />
-      </div>
-      <div className="appBody">
-        <PageContent page={page} />
-      </div>
-      <div className="appFooter">
-        <PageFooter />
-      </div>
-    </div>
+      <Routes>
+        <Route path="/" component={ BrowsingRecipesTable } />
+        <Route path="/createrecipe" component={ NewRecipeFormPage } />
+        <Route path="/editrecipe" component={ EditRecipesFormPage } />
+        <Route path="/settings" component={ SettingsPage } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/PageFooter.js
+++ src/PageFooter.js
@@ -27,41 +27,41 @@ function Roadmap(props)
         nested>{
             close => (
                 <div className="comingSoonContainer">
-                <div className="comingSoonModal">
-                    <div className="comingSoonHeader">
-                        <h1>Improvements On the Way</h1>
+                    <div className="comingSoonModal">
+                        <div className="comingSoonHeader">
+                            <h1>Improvements On the Way</h1>
+                        </div>
+                        <div className="comingSoonContent">
+                            {' '}
+                            * [entire site] making it look not terrible
+                            <br/>
+                            * [Browse All Recipes] view recipe details in an overlay
+                            <br/>
+                            * [Create New Recipe] input validation
+                            <br/>
+                            * [Edit My Recipes] button to delete recipe as a whole
+                            <br/>
+                            * [Edit My Recipes] submit edit feedback
+                            <br/>
+                            * [Edit My Recipes] input validation
+                            <br/>
+                            * [Edit My Recipes] add a new ingredient or instruction to recipe
+                            <br/>
+                            * [Settings (logged in)] heading shows username in addition to email
+                            <br/>
+                            * [Settings (logged in)] input validation
+                            <br/>
+                            * [Settings (logged in)] feedback for all credential changes
+                            <br/>
+                            * [entire site] implementing React Router
+                        </div>
+                        <div className="comingSoonModalClose">
+                            <br/>
+                            <button className="comingSoonCloseButton" onClick={ () => { console.log('modal closed '); close(); } }>
+                                close this window
+                            </button>
+                        </div>
                     </div>
-                    <div className="comingSoonContent">
-                        {' '}
-                        * [entire site] making it look not terrible
-                        <br/>
-                        * [Browse All Recipes] view recipe details in an overlay
-                        <br/>
-                        * [Create New Recipe] input validation
-                        <br/>
-                        * [Edit My Recipes] button to delete recipe as a whole
-                        <br/>
-                        * [Edit My Recipes] submit edit feedback
-                        <br/>
-                        * [Edit My Recipes] input validation
-                        <br/>
-                        * [Edit My Recipes] add a new ingredient or instruction to recipe
-                        <br/>
-                        * [Settings (logged in)] heading shows username in addition to email
-                        <br/>
-                        * [Settings (logged in)] input validation
-                        <br/>
-                        * [Settings (logged in)] feedback for all credential changes
-                        <br/>
-                        * [entire site] implementing React Router
-                    </div>
-                    <div className="comingSoonModalClose">
-                        <br/>
-                        <button className="comingSoonCloseButton" onClick={ () => { console.log('modal closed '); close(); } }>
-                            close this window
-                        </button>
-                    </div>
-                </div>
                 </div>
             )
         }</Popup>
@@ -73,12 +73,52 @@ function SkillsAndGithub(props)
     /*  Builds a modal - displays the skills/Github without disrupting the current page's state  */
     return(
         <Popup
-        trigger={<button className="comingSoonOpenButton"> test </button>}
+        trigger={<button className="skillsGithubButton"> On Display </button>}
         modal
         nested>{
             close => (
-                <div >
-                    <span>{props.number}</span>
+                <div className="skillsGithubContainer">
+                    <div className="skillsGithubModal">
+                        <div className="skillsGithubHeader">
+                            <h1>Link to the code, features present, and experience gained.</h1>
+                        </div>
+                        <div className="skillsGithubContent">
+                            {' '}
+                            <a href="https://github.com/RobMiller329/anotheronlinecookbook">Github Repository</a><span> - note: the credentials file is not included</span>
+                            <br/><br/>
+                            Features:
+                            <br/>
+                            * Demonstrates CRUD operations through creating, reading, updating, and deleting recipes.
+                            <br/>
+                            * Use interactive modals (popups) to edit recipe components.
+                            <br/>
+                            * Use the filters to search for recipes with several criteria.
+                            <br/>
+                            * Add recipe and recipe authors to your favorites.
+                            <br/>
+                            * Create notes on recipes to reference later.
+                            <br/>
+                            * User authentication through AWS Cognito.
+                            <br/><br/>
+                            Experience:
+                            <br/>
+                            * Built with the Javascript libraries React and Node.
+                            <br/>
+                            * Hosted on AWS using EC2, RDS for the database, Route53 for the domain, and Cognito for the user authentication.
+                            <br/>
+                            * Learned about writing API calls, especially calls that dynamically build SQL queries.
+                            <br/>
+                            * Learned about the different means by which data can be managed by a site (URI, props, objects, etc).
+                            <br/>
+                            * Reinforced previous lessons regarding design patterns, scalable solutions, and UX/UI design.
+                        </div>
+                        <div className="skillsGithubModalClose">
+                            <br/>
+                            <button className="skillsGithubCloseButton" onClick={ () => { console.log('modal closed '); close(); } }>
+                                close this window
+                            </button>
+                        </div>
+                    </div>
                 </div>
             )
         }</Popup>
